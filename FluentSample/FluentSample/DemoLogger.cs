cb37adaba8a2eac29daf78aa7d978cd25c65d5be
Satyajit Behera <satyajit.behera@sumerusolutions.com>
2023-11-23 22:56:37 +0530

    Add project files.

---
+++ FluentSample/FluentSample/DemoLogger.cs
@@ -0,0 +1,13 @@
+﻿namespace FluentSample;
+
+public delegate void OnLogHandler(string text);
+
+public static class DemoLogger
+{
+    public static event OnLogHandler? OnLogHandler;
+
+    public static void WriteLine(string text)
+    {
+        OnLogHandler?.Invoke(text);
+    }
+}
